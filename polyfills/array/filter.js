7413612b0a160de06194eaa6180d4fe451e8fcba
Utkarsh-Shandilya <utkarshshandilya248@gmail.com>
2025-04-02 23:00:48 +0530

    polyfill for filter

---
+++ polyfills/array/filter.js
@@ -0,0 +1,14 @@
+Array.prototype.filter = function (callback, thisArg) {
+    if (typeof callback !== 'function') throw TypeError('callback must be a function!');
+    const result = [];
+    const arr = Object(this);
+    const len = arr.length >>> 0;
+    for (let i = 0; i < len; i++) {
+        if (i in arr) {
+            if (callback.call(thisArg, arr[i], i, arr)) {
+                result.push(arr[i]);
+            }
+        }
+    }
+    return result;
+}
