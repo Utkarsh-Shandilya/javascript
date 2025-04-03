fa38f7c1fb73e35d28d8941b8cef8a8061371251
Utkarsh-Shandilya <utkarshshandilya248@gmail.com>
2025-04-03 23:17:38 +0530

    added pub sub basic class

--- pub-sub/PubSub.js
+++ pub-sub/PubSub.js
@@ -1,15 +1,66 @@
 class PubSub {
     constructor() {
         this.events = [];
+        this.isCompleted = false;
+        this.hasError = false;
     }
 
-    subscribe(callback) {
-        this.events.push(callback);
+    subscribe(callback, onError = null, onComplete = null, ...operators) {
+        if (this.isCompleted || this.hasError)
+            return;
+        const subscriber = {
+            callback,
+            onError,
+            onComplete,
+            operators
+        };
+        this.events.push(subscriber);
+        return () => this.unSubscribe(subscriber);
     }
 
     next(newValue) {
-        this.events.forEach(func => {
-            func(newValue);
-        })
+        if (this.isCompleted || this.hasError)
+            return;
+        this.events.forEach(({ callback, operators }) => {
+            try {
+                const processedValue = (operators || []).reduce((val, op) => op(val), newValue);
+                callback(processedValue);
+            } catch (error) {
+                this.error(error);
+            }
+        }
+        );
+    }
+
+    error(err) {
+        if (this.isCompleted || this.hasError)
+            return;
+        this.hasError = true;
+        this.events.forEach(({ onError }) => {
+            if (onError)
+                onError(err);
+        }
+        );
+        this.clearSubscriptions();
+    }
+
+    complete() {
+        if (this.isCompleted || this.hasError)
+            return;
+        this.isCompleted = true;
+        this.events.forEach(({ onComplete }) => {
+            if (onComplete)
+                onComplete();
+        }
+        );
+        this.clearSubscriptions();
+    }
+
+    unSubscribe(callback) {
+        this.events = this.events.filter(event => event.callback !== callback);
+    }
+
+    clearSubscriptions() {
+        this.events = [];
     }
 }
