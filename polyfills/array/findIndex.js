ccc90142d54ae84e5e858703875363ca3db64aa1
Utkarsh-Shandilya <utkarshshandilya248@gmail.com>
2025-04-03 23:30:47 +0530

    polyfill for findIndex()

---
+++ polyfills/array/findIndex.js
@@ -0,0 +1,13 @@
+Array.prototype.findIndex = function (callback, thisArg) {
+    if (typeof callback !== 'function') throw TypeError('callback must be a function!');
+    const arr = Object(this);
+    const len = arr.length >>> 0;
+    for (let i = 0; i < len; i++) {
+        if (i in arr) {
+            if (callback.call(thisArg, arr[i], i, arr)) {
+                return i;
+            }
+        }
+    }
+    return -1;
+}
