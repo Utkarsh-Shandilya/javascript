27a6206258ac84adce9c6db72e4dad36fa2f7df2
Utkarsh-Shandilya <utkarshshandilya248@gmail.com>
2025-04-03 23:30:08 +0530

    polyfill for every()

---
+++ polyfills/array/every.js
@@ -0,0 +1,13 @@
+Array.prototype.every = function (callback, thisArg) {
+    if (typeof callback !== 'function') throw TypeError('callback must be a function!');
+    const arr = Object(this);
+    const len = arr.length >>> 0;
+    for (let i = 0; i < len; i++) {
+        if (i in arr) {
+            if (!callback.call(thisArg, arr[i], i, arr)) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
